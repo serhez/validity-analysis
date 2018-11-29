a3948ae71c160dfc7d2da8171d3f26632116583d
Hezser <contact.sergiohernandez@gmail.com>
2018-11-29 01:19:04 +0000

    Have run more analyses.

--- src/Analysis.java
+++ src/Analysis.java
@@ -11,68 +11,29 @@ public class Analysis {
     // TODO: [cont.] files used are those in this module. When doing this using a main method instead, the path is
     // TODO: [cont.] that of the project. FIX.
 
-    // Less than 50 chars formulas, system K
+    // Size 30, system K
     @Test
-    public void numberOfPropositions() throws InvalidNumberOfPropositionsException, IOException {
-        InputGenerator inputGenerator = new InputGenerator();
-        ArrayList<Double> validityProportions = new ArrayList<>();  // Validity Proportion of Element = (# valid formulas) / (# formulas)
-
+    public void numberOfPropositions() throws InvalidNumberOfPropositionsException, IOException, UnrecognizableFormulaException {
+        Prover prover = new Prover(false);
+        System.out.println("----- Starting analysis based on the number of propositions -----");
         for (int maxPropositions = 1; maxPropositions <= 26; maxPropositions++) {
-            inputGenerator.generateInputFile(10000, 50, maxPropositions, "K");
-            Prover prover = new Prover(false);
-            prover.proveInputFile();
-            String rawOutput = prover.readOutputFile();
-            ArrayList<Integer> output = parseOutput(rawOutput);
-            validityProportions.add(calculateValidityProportion(output));
-        }
-
-        System.out.println(1 + " proposition: " + (validityProportions.get(0)*100) + "%");
-        for (int i = 2; i <= 26; i++) {
-            System.out.println(i + " propositions: " + (validityProportions.get(i-1)*100) + "%");
+            double validityProportion = calculateValidityProportion(prover.proveRandomFormulas(100000, 30, maxPropositions, new ModalSystem("K")));
+            write((maxPropositions + " propositions: " + (validityProportion*100) + "%\n"), "results/number_of_propositions.txt");
+            System.out.println(maxPropositions + " propositions completed");
         }
     }
 
     // 2 propositions, system K
     @Test
-    public void sizeOfFormula() throws InvalidNumberOfPropositionsException, IOException {
-        InputGenerator inputGenerator = new InputGenerator();
-        ArrayList<Double> validityProportions = new ArrayList<>();  // Validity Proportion of Element = (# valid formulas) / (# formulas)
+    public void sizeOfFormula() throws InvalidNumberOfPropositionsException, IOException, UnrecognizableFormulaException {
         Prover prover = new Prover(false);
-
-        System.out.println("----- Starting Analysis based on the size of formulas -----");
-        for (int size = 28; size <= 30; size++) {
-            inputGenerator.generateInputFile(100000, size, 2, "K");
-            prover.proveInputFile();
-            String rawOutput = prover.readOutputFile();
-            validityProportions.add(calculateValidityProportion(parseOutput(rawOutput)));
-            write(("Size of " + size + ": " + (calculateValidityProportion(parseOutput(rawOutput))*100) + "%\n"), "results/size_of_formula.txt");
+        System.out.println("----- Starting analysis based on the size of formulas -----");
+        for (int size = 61; size <= 70; size++) {
+            double validityProportion = calculateValidityProportion(prover.proveRandomFormulas(100000, size, 2, new ModalSystem("K")));
+            write(("Size of " + size + ": " + (validityProportion*100) + "%\n"), "results/size_of_formula.txt");
             System.out.println("Size " + size + " completed");
         }
     }
-
-    // Does not work due to insufficient heap space
-//    // 2 propositions, system K
-//    @Test
-//    public void sizeOfFormula() throws InvalidNumberOfPropositionsException, IOException {
-//        InputGenerator inputGenerator = new InputGenerator();
-//        ArrayList<Double> validityProportions = new ArrayList<>();  // Validity Proportion of Element = (# valid formulas) / (# formulas)
-//        Prover prover = new Prover(false);
-//
-//        System.out.println("----- Starting Analysis based on the size of formulas -----");
-//        for (int size = 1; size <= 100; size++) {
-//            inputGenerator.generateInputFile(1000, size, 2, "K");
-//            prover.proveInputFile();
-//            String rawOutput = prover.readOutputFile();
-//            validityProportions.add(calculateValidityProportion(parseOutput(rawOutput)));
-//            System.out.println(size + " % completed");
-//        }
-//
-//        System.out.println("\n----- Results -----");
-//        System.out.println("Size of " + 1 + ": " + (validityProportions.get(0)*100) + "%");
-//        for (int i = 2; i <= 100; i++) {
-//            System.out.println("Size of " + i + ": " + (validityProportions.get(i-1)*100) + "%");
-//        }
-//    }
 
     private static double calculateValidityProportion(ArrayList<Integer> output) {
         int validFormulas = 0;
@@ -83,28 +44,6 @@ public class Analysis {
         }
         double proportion = ((double)validFormulas)/((double)(output.size()));
         return proportion;
-    }
-
-    // 1 = valid; 0 = invalid
-    private static ArrayList<Integer> parseOutput(String rawOutput) {
-
-        ArrayList<Integer> output = new ArrayList<>();
-
-        for (int c = 0; c < rawOutput.length() - 4; c++) {
-            if (rawOutput.charAt(c) == 'v' && rawOutput.charAt(c+1) == 'a' && rawOutput.charAt(c+2) == 'l' && rawOutput.charAt(c+3) == 'i' && rawOutput.charAt(c+4) == 'd') {
-                if (c < 4) {
-                    output.add(1);
-                    continue;
-                }
-                if (rawOutput.charAt(c-4) == 'n' && rawOutput.charAt(c-3) == 'o' && rawOutput.charAt(c-2) == 't' && rawOutput.charAt(c-1) == ' ') {
-                    output.add(0);
-                } else {
-                    output.add(1);
-                }
-            }
-        }
-
-        return output;
     }
 
     private void write(String string, String path) throws IOException {
