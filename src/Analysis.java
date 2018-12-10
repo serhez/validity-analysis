323391304ed5fc7c877d86259c1290cf3b3b7915
Hezser <contact.sergiohernandez@gmail.com>
2018-12-10 01:18:29 +0000

    Small formatting changes (names of methods).

--- src/Analysis.java
+++ src/Analysis.java
@@ -7,15 +7,11 @@ import java.util.ArrayList;
 
 public class Analysis {
 
-    // TODO:         By doing a test, the running path is this module (.../Validity-Analysis) so that the input and output
-    // TODO: [cont.] files used are those in this module. When doing this using a main method instead, the path is
-    // TODO: [cont.] that of the project. FIX.
-
     // Size 30, system K
     @Test
     public void numberOfPropositions() throws InvalidNumberOfPropositionsException, IOException, UnrecognizableFormulaException {
         Prover prover = new Prover(false);
-        System.out.println("----- Starting analysis based on the number of propositions -----");
+        System.out.println("----- Starting analysis based on the number of propositions -----\n");
         for (int maxPropositions = 1; maxPropositions <= 26; maxPropositions++) {
             double validityProportion = calculateValidityProportion(prover.proveRandomFormulas(100000, 30, maxPropositions, new ModalSystem("K")));
             write((maxPropositions + " propositions: " + (validityProportion*100) + "%\n"), "results/number_of_propositions.txt");
@@ -27,11 +23,11 @@ public class Analysis {
     @Test
     public void sizeOfFormula() throws InvalidNumberOfPropositionsException, IOException, UnrecognizableFormulaException {
         Prover prover = new Prover(false);
-        System.out.println("----- Starting analysis based on the size of formulas -----");
-        for (int size = 61; size <= 70; size++) {
+        System.out.println("----- Starting analysis based on the size of formulas -----\n");
+        for (int size = 100; size <= 100; size++) {
             double validityProportion = calculateValidityProportion(prover.proveRandomFormulas(100000, size, 2, new ModalSystem("K")));
-            write(("Size of " + size + ": " + (validityProportion*100) + "%\n"), "results/size_of_formula.txt");
-            System.out.println("Size " + size + " completed");
+            write(("Size of " + size + ": " + (validityProportion * 100) + "%\n"), "results/size_of_formula.txt");
+            System.out.println("\n-- Size " + size + " completed --\n");
         }
     }
 
