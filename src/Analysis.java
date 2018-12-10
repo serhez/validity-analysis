fb85a8c944b4de4ab05e34c3964a1d8097cd7746
Hezser <contact.sergiohernandez@gmail.com>
2018-12-10 20:16:49 +0000

    Made the random generator more efficient.

--- src/Analysis.java
+++ src/Analysis.java
@@ -7,21 +7,31 @@ import java.util.ArrayList;
 
 public class Analysis {
 
-    // Size 30, system K
-    @Test
-    public void numberOfPropositions() throws InvalidNumberOfPropositionsException, IOException, UnrecognizableFormulaException {
-        Prover prover = new Prover(false);
-        System.out.println("----- Starting analysis based on the number of propositions -----\n");
-        for (int maxPropositions = 1; maxPropositions <= 26; maxPropositions++) {
-            double validityProportion = calculateValidityProportion(prover.proveRandomFormulas(100000, 30, maxPropositions, new ModalSystem("K")));
-            write((maxPropositions + " propositions: " + (validityProportion*100) + "%\n"), "results/number_of_propositions.txt");
-            System.out.println(maxPropositions + " propositions completed");
-        }
-    }
+//    // Size 30, system K
+//    @Test
+//    public void numberOfPropositions() throws InvalidNumberOfPropositionsException, IOException, UnrecognizableFormulaException {
+//        Prover prover = new Prover(false);
+//        System.out.println("----- Starting analysis based on the number of propositions -----\n");
+//        for (int maxPropositions = 1; maxPropositions <= 26; maxPropositions++) {
+//            double validityProportion = calculateValidityProportion(prover.proveRandomFormulas(100000, 30, maxPropositions, new ModalSystem("K")));
+//            write((maxPropositions + " propositions: " + (validityProportion*100) + "%\n"), "results/number_of_propositions.txt");
+//            System.out.println(maxPropositions + " propositions completed");
+//        }
+//    }
 
-    // 2 propositions, system K
-    @Test
-    public void sizeOfFormula() throws InvalidNumberOfPropositionsException, IOException, UnrecognizableFormulaException {
+//    // 2 propositions, system K
+//    @Test
+//    public void sizeOfFormula() throws InvalidNumberOfPropositionsException, IOException, UnrecognizableFormulaException {
+//        Prover prover = new Prover(false);
+//        System.out.println("----- Starting analysis based on the size of formulas -----\n");
+//        for (int size = 100; size <= 100; size++) {
+//            double validityProportion = calculateValidityProportion(prover.proveRandomFormulas(100000, size, 2, new ModalSystem("K")));
+//            write(("Size of " + size + ": " + (validityProportion * 100) + "%\n"), "results/size_of_formula.txt");
+//            System.out.println("\n-- Size " + size + " completed --\n");
+//        }
+//    }
+
+    public static void main(String[] args) throws InvalidNumberOfPropositionsException, IOException, UnrecognizableFormulaException {
         Prover prover = new Prover(false);
         System.out.println("----- Starting analysis based on the size of formulas -----\n");
         for (int size = 100; size <= 100; size++) {
@@ -42,7 +52,7 @@ public class Analysis {
         return proportion;
     }
 
-    private void write(String string, String path) throws IOException {
+    private static void write(String string, String path) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(path, true));
         writer.write(string);
         writer.close();
